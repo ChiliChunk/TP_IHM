2b08951c8b510dfdfbc6233f35b42db9cd9ae14e
Nicolas <fussure@gmail.com>
2020-09-23 11:23:35 +0200

    fin du tp

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,18 @@ import fr.dgac.ivy.*;
 
 public class Main {
     public static void main(String[] args) {
-        Ivy bus =  new Ivy("myAgent", "Ceci est un message", null);
+        Ivy bus =  new Ivy("myAgent", "Palette:CreerRectangle", null);
 
         try{
             bus.start("127.255.255.255:2010");
-            bus.sendMsg("Test");
-            bus.bindMsg("Palette:(.*)", new IvyMessageListener() {
+            bus.bindMsg("Palette:MouseMoved(.*)", new IvyMessageListener() {
                 @Override
                 public void receive(IvyClient client, String[] args) {
-                    System.out.println("agent : " + client.getApplicationName());
-                    System.out.println(args[0]);
+//                    System.out.println("agent : " + client.getApplicationName());
+                    System.out.println("Le  pointeur  de  la  souris  se  trouve  à  la position ("+ args[0]+")");
                 }
             });
-            System.out.println("envoyé");
-
+//            bus.sendMsg("Palette:CreerRectangle");
         }catch (Exception e){
             System.out.println("In Catch");
             System.out.println(e.getMessage());
