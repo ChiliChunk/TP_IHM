db81d4591a886ed0af330e4858a03b34d3064dc2
Nicolas <fussure@gmail.com>
2020-09-24 10:12:41 +0200

    Exo 1 tp 2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
 import fr.dgac.ivy.*;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        Ivy bus =  new Ivy("myAgent", "Palette:CreerRectangle", null);
+        Ivy bus =  new Ivy("myAgent", "", null);
 
         try{
             bus.start("127.255.255.255:2010");
-            bus.bindMsg("Palette:MouseMoved(.*)", new IvyMessageListener() {
+            bus.bindMsg("Palette:MousePressed(.*)", new IvyMessageListener() {
                 @Override
                 public void receive(IvyClient client, String[] args) {
-//                    System.out.println("agent : " + client.getApplicationName());
-                    System.out.println("Le  pointeur  de  la  souris  se  trouve  à  la position ("+ args[0]+")");
+                    try{
+                        System.out.println(args[0]);
+                        bus.sendMsg("Palette:CreerEllipse" +args[0] + " longueur=1 hauteur=1 couleurContour=green" );
+                    }catch (Exception e){
+                        System.out.println("Catch mouse pressed");
+                    }
+                }
+            } );
+            bus.bindMsg("Palette:MouseDragged(.*)", new IvyMessageListener() {
+                @Override
+                public void receive(IvyClient client, String[] args) {
+                    try{
+                        bus.sendMsg("Palette:CreerEllipse" +args[0] + " longueur=1 hauteur=1 couleurContour=220:220:220" );
+                    }
+                    catch (Exception e){
+
+                    }
+
                 }
             });
-//            bus.sendMsg("Palette:CreerRectangle");
+
+            bus.bindMsg("Palette:MouseReleased(.*)", new IvyMessageListener() {
+                @Override
+                public void receive(IvyClient client, String[] args) {
+                    try{
+                        bus.sendMsg("Palette:CreerEllipse" +args[0] + " longueur=1 hauteur=1 couleurContour=red" );
+                    }catch (Exception e){
+
+                    }
+                }
+            });
         }catch (Exception e){
             System.out.println("In Catch");
             System.out.println(e.getMessage());
